142ed0da499988b179025dc249980ec6f928d435
SergioViacheslaev <internetwalker@yandex.ru>
2021-08-13 14:48:18 +0300

    Refactor registration controller

--- services-status-checker/src/main/kotlin/com/servicesmonitor/servicesstatuschecker/controller/ServiceRegistrationController.kt
+++ services-status-checker/src/main/kotlin/com/servicesmonitor/servicesstatuschecker/controller/ServiceRegistrationController.kt
@@ -5,13 +5,15 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
+@RequestMapping("/services-monitor")
 class ServiceRegistrationController {
 
-    @PostMapping("/services-monitor/services-reg")
+    @PostMapping("/services-reg")
     fun handleRegistranionServiceRequest(@RequestBody monitoredService: MonitoredService): ResponseEntity<String> {
         return ResponseEntity("Service registered", HttpStatus.OK)
     }
