9d6e171d693dcd30685c2330e492bdcf6583e48e
SergioViacheslaev <internetwalker@yandex.ru>
2021-08-30 16:59:48 +0300

    Add Controller exception handler

---
+++ services-status-monitor-service/src/main/kotlin/com/servicesmonitor/servicesstatusmonitorservice/controller/ControllerExceptionHandler.kt
@@ -0,0 +1,20 @@
+package com.servicesmonitor.servicesstatusmonitorservice.controller
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+
+@RestControllerAdvice(assignableTypes = [ServiceRegistrationController::class])
+class ControllerExceptionHandler {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @ExceptionHandler(value = [HttpMessageNotReadableException::class])
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    fun handleServiceRegistrationDataParseError() {
+        logger.warn("Service registration data JSON parse error")
+    }
+}
