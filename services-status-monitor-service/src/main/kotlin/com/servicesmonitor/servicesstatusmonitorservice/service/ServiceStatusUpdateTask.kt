6117b587650d9362436a2f5ef1a18e35bebbdefe
SergioViacheslaev <internetwalker@yandex.ru>
2021-08-31 15:03:59 +0300

    Uncomment scheduled task

--- services-status-monitor-service/src/main/kotlin/com/servicesmonitor/servicesstatusmonitorservice/service/ServiceStatusUpdateTask.kt
+++ services-status-monitor-service/src/main/kotlin/com/servicesmonitor/servicesstatusmonitorservice/service/ServiceStatusUpdateTask.kt
@@ -1,6 +1,7 @@
 package com.servicesmonitor.servicesstatusmonitorservice.service
 
 import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 /**
@@ -12,7 +13,7 @@ class ServiceStatusUpdateTask(
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-//    @Scheduled(fixedRate = 5_000)
+    @Scheduled(fixedRate = 5_000)
     fun updateServiceStatusTask() {
         logger.info("Status update task is performed")
         statusUpdateService.updateServicesStatus()
